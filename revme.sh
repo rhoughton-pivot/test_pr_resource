ad7b22d975fca5640db1abbbd4d8d0beaf5c3fda
Robert Houghton <rhoughton@pivotal.io>
2018-01-11 13:42:54 -0800

    disable fetch-merge and add test script

---
+++ revme.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+
+set +x
+
+ver=`./incr.sh`
+git reset --hard origin/master
+
+git checkout master && \
+    git checkout -b f${ver} -t origin/master && \
+    echo ${ver} > verfile && \
+    git add -u && git commit -m "${ver}" && git push origin f${ver} && \
+    \
+hub pull-request -m "$(git log -1 --pretty=%B)";
+
