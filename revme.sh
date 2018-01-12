2b8efdffcfdaca83bdff3ebd8965744c2934a229
Robert Houghton <rhoughton@pivotal.io>
2018-01-11 16:26:17 -0800

    fixme

--- revme.sh
+++ revme.sh
@@ -9,7 +9,7 @@ git reset --hard origin/master
 git checkout master && \
     git checkout -b f${ver} -t origin/master && \
     echo ${ver} > verfile && \
-    git add -u && git commit -m "${ver}" && git push origin f${ver} && \
+    git add -u && git commit -m "${ver}" && git push origin f${ver}
 
 
 
